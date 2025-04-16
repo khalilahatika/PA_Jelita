2020b4ee1d03c82f0a79a507ca2ee44ee5546a82
Khalilah Atik Akmal <khalilahatikaakmal@gmail.com>
2025-04-17 06:06:19 +0700

    Tampilan main

--- app/src/main/java/com/google/mediapipe/examples/poselandmarker/MainActivity.kt
+++ app/src/main/java/com/google/mediapipe/examples/poselandmarker/MainActivity.kt
@@ -34,13 +34,13 @@ class MainActivity : AppCompatActivity() {
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
-        val navController = navHostFragment.navController
-        activityMainBinding.navigation.setupWithNavController(navController)
-        activityMainBinding.navigation.setOnNavigationItemReselectedListener {
-            // ignore the reselection
-        }
+//        val navHostFragment =
+//            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+//        val navController = navHostFragment.navController
+//        activityMainBinding.navigation.setupWithNavController(navController)
+//        activityMainBinding.navigation.setOnNavigationItemReselectedListener {
+//            // ignore the reselection
+//        }
     }
 
     override fun onBackPressed() {
